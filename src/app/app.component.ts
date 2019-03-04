6daa1b94f64cefedac7ec60f6c5be4f085bda555
Héctor J. Vásquez <ipi.vasquez@gmail.com>
2019-03-03 19:23:52 -0600

    Angular + Nest Integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nest-angular';
+  apiMessage: string;
+  failed = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('/api').subscribe(
+      (res: any) => (this.apiMessage = res.txt),
+      err => {
+        this.failed = true;
+        console.error(err);
+      },
+    );
+  }
 }
